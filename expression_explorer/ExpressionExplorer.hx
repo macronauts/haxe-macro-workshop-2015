50e5576b8d82e3a17042f34ca96bb47cc01dff45
Jason O'Neil <jason.oneil@gmail.com>
2015-05-31 18:49:26 +0200

    Add ExpressionExplorer

---
+++ expression_explorer/ExpressionExplorer.hx
@@ -0,0 +1,31 @@
+import haxe.macro.Expr;
+using haxe.macro.Tools;
+
+// Run with `haxe --run ExpressionExplorer`
+
+class ExpressionExplorer {
+
+	static function main() {
+		var myExpression = macro function() return null;
+
+		// Trace the expression, showing the enums and objects it is made up of.
+		trace( myExpression.expr );
+
+		// Trace the expression, using the ExprTools.toString() to show what the source looks like.
+		trace( myExpression.toString() );
+
+		// Do some switching using enums.
+		switch myExpression.expr {
+			case EVars([varSetup]):
+				trace('The variable name is: '+varSetup.name);
+			default:
+		}
+
+		// Do some switching using reification.
+		switch myExpression {
+			case macro var x = $value:
+				trace('The value is '+value.toString());
+			default:
+		}
+	}
+}
